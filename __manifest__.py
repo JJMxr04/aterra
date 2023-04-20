2b448ab9ffff02bcf6d2cdad055659514935241e
Joe <maiaranagaming@gmail.com>
2023-04-20 18:42:18 +0000

    Trading Card Web Block

--- __manifest__.py
+++ __manifest__.py
@@ -1,35 +1,46 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Aterra",
-
-    'summary': """""",
-
-    'description': """
-        
-    """,
+    'version': '1.0',
+    'description': "Aterra Card Game",
+    'summary': "Aterra Card Game",
 
     'author': "Joseph Maiarana",
-    'website': "",
+    'website': "www.WarforAterra.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'App',
-    'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'web'],
 
     # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/aterra_card.xml',
+    ],
+    'qweb': [
+        'static/src/xml/aterra_card.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+    'images': [
+        'aterra/static/description/Aterra_Logo_Nebula_Back_White_1.1.png',
+    ],
     'installable': True,
     'application': True,
+    'installable': True,
+    'auto_install': False,
+
+    'assets': {
+        'web.assets_backend': [
+            "aterra/static/src/css/aterra_card.css",
+        ]
+    },
 }
