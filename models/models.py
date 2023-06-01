b2287af11e75b5ad806adc826195b15efd95fe79
Joseph Maiarana <maiaranagaming@gmail.com>
2023-06-01 12:18:48 -0400

    Type Model, views actions and menu items work, along with some cosmetics

--- models/models.py
+++ models/models.py
@@ -5,21 +5,24 @@ from odoo import models, fields, api
 class Aterra(models.Model):
     _name = 'aterra.aterra'
     _description = 'aterra.aterra'
-    api_id = fields.Char(compute="def_api_id", store =True)
-    name = fields.Char()
-    volume = fields.Integer()
-    series = fields.Char()
-    card_number_id = fields.Integer()
-    rarity = fields.Char()
-    rank = fields.Integer()
-    power = fields.Integer()
-    defense = fields.Integer()
+    name = fields.Char(string = 'Name')
+    volume = fields.Integer(string = 'Volume')
+    series = fields.Char(string = 'Series')
+    card_number_id = fields.Integer(string = 'Card Number ID')
+    type = fields.Many2one('aterra.cardtype', string='Card Type')
+    rarity = fields.Many2one('aterra.cardrarity', string='Card Rarity')
+    rank = fields.Integer(string = 'Rank')
+    power = fields.Integer(string = 'Power')
+    defense = fields.Integer(string = 'Defense')
     elements = fields.Char()
-    abilities = fields.Text()
-    description = fields.Text()
-    image_Attachment_name = fields.Char(compute="def_att_name", store =True)  
+    abilities = fields.Text(string = 'Abilities')
+    description = fields.Text(string = 'Description')
+    api_id = fields.Char(compute="def_api_id", store =True)
+    image_Attachment_name = fields.Char(compute="def_att_name", store =True) 
     image = fields.Binary(string="Image")
-    image_url = fields.Char(compute='_compute_image_url', store=True)  # Added store=True to store the computed URL
+    image_url = fields.Char(compute='_compute_image_url', store=True, string = 'Card Photo')  # Added store=True to store the computed URL
+    rarity_image_url = fields.Char(compute='_get_rarity_image_url', store=True, string = 'Rarity Image URL')
+    type_image_url = fields.Char(compute='_get_type_image_url', store=True, string = 'Type Image URL')
     
     
     @api.model
@@ -86,8 +89,24 @@ class Aterra(models.Model):
             else:
                 record.image_Attachment_name =  'Image Name'
 
+    @api.depends('rarity')
+    def _get_rarity_image_url(self):
+        for record in self:
+            if record.rarity:
+                record.rarity_image_url = record.rarity.image_url
+            else:
+                record.rarity_image_url = False
+
+    @api.depends('type')
+    def _get_type_image_url(self):
+        for record in self:
+            if record.type:
+                record.type_image_url = record.type.image_url
+            else:
+                record.type_image_url = False
 
 
+#________________________________________________________________________-
 class AterraCardRarity(models.Model):
     _name = 'aterra.cardrarity'
     _description = 'aterra.cardrarity'
@@ -134,4 +153,48 @@ class AterraCardRarity(models.Model):
                 record.image_url = False
             
     
+#________________________________________________________________________-
+class AterraCardType(models.Model):
+    _name = 'aterra.cardtype'
+    _description = 'aterra.cardtype'
+    name = fields.Char()
+    # rarity = fields.Char()
+    image = fields.Binary(string="Image")
+    image_url = fields.Char(compute='_compute_image_url', store=True)  # Added store=True to store the computed URL
 
+    @api.model
+    def create(self, values):
+        # Create a new record
+        record = super(AterraCardType, self).create(values)
+
+        # Check if an image is provided
+        if 'image' in values:
+            # Attach the image as an attachment
+            attachment = self.env['ir.attachment'].create({
+                'name': record.name,
+                'type': 'binary',
+                'datas': values['image'],
+                'res_model': 'aterra.cardtype',
+                'res_id': record.id,
+            })
+
+
+        return record
+    
+    @api.depends('image','name')
+    def _compute_image_url(self):
+        base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
+        for record in self:
+            if record.image:
+                # Generate the attachment URL based on the attachment ID and file name
+                attachment_id = self.env['ir.attachment'].search([
+                    ('res_model', '=', 'aterra.cardtype'),
+                    ('res_id', '=', record.id),
+                    ('name', '=', record.name)
+                ], limit=1)
+                if attachment_id:
+                    record.image_url = '{}/web/content/{}/{}'.format(base_url, attachment_id.id, attachment_id.name)
+                else:
+                    record.image_url = False
+            else:
+                record.image_url = False
