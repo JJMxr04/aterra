8bdeae2a20800d1a6fcd90e70b65f7d92bf05372
Joseph Maiarana <maiaranagaming@gmail.com>
2023-06-01 16:49:35 -0400

    Element and contract Model, views actions and menu items work, along with some cosmetics

--- models/models.py
+++ models/models.py
@@ -14,21 +14,23 @@ class Aterra(models.Model):
     rank = fields.Integer(string = 'Rank')
     power = fields.Integer(string = 'Power')
     defense = fields.Integer(string = 'Defense')
-    elements = fields.Char(compute='_get_list_of_elememts', store=True, string = 'Elements')
     element1 = fields.Many2one('aterra.cardelement', string='1st Card Element')
     element2 = fields.Many2one('aterra.cardelement', string='2nd Card Element')
     element3 = fields.Many2one('aterra.cardelement', string='3rd Card Element')
     element4 = fields.Many2one('aterra.cardelement', string='4th Card Element')
     element5 = fields.Many2one('aterra.cardelement', string='5th Card Element')
     element6 = fields.Many2one('aterra.cardelement', string='6th Card Element')
+    contract = fields.Many2one('aterra.cardcontract', string='Contract')
     abilities = fields.Text(string = 'Abilities')
     description = fields.Text(string = 'Description')
+    image = fields.Binary(string="Image")
     api_id = fields.Char(compute="def_api_id", store =True)
     image_Attachment_name = fields.Char(compute="def_att_name", store =True) 
-    image = fields.Binary(string="Image")
+    elements = fields.Char(compute='_get_list_of_elememts', store=True, string = 'Elements')
     image_url = fields.Char(compute='_compute_image_url', store=True, string = 'Card Photo')  # Added store=True to store the computed URL
     rarity_image_url = fields.Char(compute='_get_rarity_image_url', store=True, string = 'Rarity Image URL')
     type_image_url = fields.Char(compute='_get_type_image_url', store=True, string = 'Type Image URL')
+    contract_image_url = fields.Char(compute='_get_contract_image_url', store=True, string = 'Contract Image URL')
     element1_image_url = fields.Char(compute='_get_element1_image_url', store=True, string = '1st Element Image URL')
     element2_image_url = fields.Char(compute='_get_element2_image_url', store=True, string = '2nd Element Image URL')
     element3_image_url = fields.Char(compute='_get_element3_image_url', store=True, string = '3rd Element Image URL')
@@ -184,6 +186,14 @@ class Aterra(models.Model):
             if record.element6:
                 element_list_string += " " + record.element6.name
         record.elements = element_list_string
+
+    @api.depends('contract')
+    def _get_contract_image_url(self):
+        for record in self:
+            if record.contract:
+                record.contract_image_url = record.contract.image_url
+            else:
+                record.contract_image_url = False
                     
 
 
@@ -319,6 +329,53 @@ class AterraCardElement(models.Model):
                 # Generate the attachment URL based on the attachment ID and file name
                 attachment_id = self.env['ir.attachment'].search([
                     ('res_model', '=', 'aterra.cardelement'),
+                    ('res_id', '=', record.id),
+                    ('name', '=', record.name)
+                ], limit=1)
+                if attachment_id:
+                    record.image_url = '{}/web/content/{}/{}'.format(base_url, attachment_id.id, attachment_id.name)
+                else:
+                    record.image_url = False
+            else:
+                record.image_url = False
+
+
+#________________________________________________________________________-
+class AterraCardContract(models.Model):
+    _name = 'aterra.cardcontract'
+    _description = 'aterra.cardcontract'
+    name = fields.Char()
+    # rarity = fields.Char()
+    image = fields.Binary(string="Image")
+    image_url = fields.Char(compute='_compute_image_url', store=True)  # Added store=True to store the computed URL
+
+    @api.model
+    def create(self, values):
+        # Create a new record
+        record = super(AterraCardContract, self).create(values)
+
+        # Check if an image is provided
+        if 'image' in values:
+            # Attach the image as an attachment
+            attachment = self.env['ir.attachment'].create({
+                'name': record.name,
+                'type': 'binary',
+                'datas': values['image'],
+                'res_model': 'aterra.cardcontract',
+                'res_id': record.id,
+            })
+
+
+        return record
+    
+    @api.depends('image','name')
+    def _compute_image_url(self):
+        base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
+        for record in self:
+            if record.image:
+                # Generate the attachment URL based on the attachment ID and file name
+                attachment_id = self.env['ir.attachment'].search([
+                    ('res_model', '=', 'aterra.cardcontract'),
                     ('res_id', '=', record.id),
                     ('name', '=', record.name)
                 ], limit=1)
