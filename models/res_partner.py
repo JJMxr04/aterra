71df558d110231a95fa71e89831406913ffb1eeb
Joseph Maiarana <maiaranagaming@gmail.com>
2023-07-11 22:44:05 -0400

    Added coins to res.partner

--- models/res_partner.py
+++ models/res_partner.py
@@ -3,8 +3,4 @@ from odoo import models, fields, api, _
 class AterraPartner(models.Model):
     _inherit = 'res.partner'
 
-    # custom_field = fields.Char(string='Custom Field')
     coins = fields.Integer(string='Coins')
-
-
-#     # coins = volume = fields.Integer(string = 'Coins')
